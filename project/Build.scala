94d1d969bf43e59200f1d07b37eeaa331001fcef
zoosky <zoosky@gmail.com>
2016-08-12 14:45:15 +0200

    update to lastest Scala and ScalaTest

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,11 @@ object Build extends Build {
   lazy val buildSettings = Seq(
     organization := "io.gatling.funspec",
     version := "1.0.0",
-    scalaVersion := "2.11.6"
+    scalaVersion := "2.11.8"
   )
 
   lazy val projectDependencies = Seq(
-    "org.scalatest"                   %% "scalatest"                  % "2.2.3"          % "test,it",
+    "org.scalatest"                   %% "scalatest"                  % "3.0.0"          % "test,it",
     "io.gatling.highcharts"           % "gatling-charts-highcharts"   % "2.2.0"          % "test,it",
     "io.gatling"                      % "gatling-test-framework"      % "2.2.0"          % "test,it"
   )
